84b80a75873f7df5eb352874a91490a74098bb4e
krishna-Rai <38042014+krishna-Rai@users.noreply.github.com>
2022-02-27 23:00:15 +0530

    Create BinaryPreifx.java

---
+++ BinaryPreifx.java
@@ -0,0 +1,19 @@
+//Find all the n bit binary numbers having more 1's than o's
+class Solution {
+    public List<String> permute(String s,int start,int end,String op,List<String> ans){
+        if(start>end){
+            ans.add(op);
+            return ans;
+        }
+        if(Character.isDigit(s.charAt(start))){
+            permute(s,start+1,end,op+s.charAt(start),ans);
+        }else{
+            permute(s,start+1,end,op+Character.toLowerCase(s.charAt(start)),ans);
+            permute(s,start+1,end,op+Character.toUpperCase(s.charAt(start)),ans);
+        }
+        return ans;
+    }
+    public List<String> letterCasePermutation(String s) {
+        return permute(s,0,s.length()-1,"",new ArrayList<String>());
+    }
+}
